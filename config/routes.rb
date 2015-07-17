e270e5577f96071f554ff87ec7eed4c31d009535
Jason Denney <jdenney@lgscout.com>
2015-07-17 11:24:43 -0500

    WIP, starting API version controller

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  namespace :api do
+    scope module: :v1 do
+    end
+  end
 end
