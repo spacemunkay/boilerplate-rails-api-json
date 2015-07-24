b428ce55322ba67c14270024d8e9609d03bf3269
Jason Denney <jdenney@lgscout.com>
2015-07-24 11:05:31 -0500

    Enable auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,7 @@ class ApplicationController < ActionController::API
   include ActionController::MimeResponds
   include ActionController::ImplicitRender
 
+  acts_as_token_authentication_handler_for User
+
   respond_to :json
 end
