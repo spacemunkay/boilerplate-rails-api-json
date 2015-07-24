450c92e67574f09919b5964747dd91e9b33a1938
Jason Denney <jdenney@lgscout.com>
2015-07-24 10:54:35 -0500

    Switch JSON rendering for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::API
   # Necessary for gem: active_model_serializers
-  include ActionController::Serialization
+  # include ActionController::Serialization
+
+  include ActionController::MimeResponds
+  include ActionController::ImplicitRender
+
+  respond_to :json
 end
